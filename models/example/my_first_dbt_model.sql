0ab129e00b76ff064681d99f271557f83a26c494
geniusmotorlab@gmail.com <geniusmotorlab@gmail.com>
2023-10-06 15:05:54 +0000

    first and second remove

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,4 +1,6 @@
 
+
+
 /*
     Welcome to your first dbt model!
     Did you know that you can also configure models directly within SQL files?
@@ -6,7 +8,8 @@
 
     Try changing "table" to "view" below
 */
-/* comment added here - Azhar
+
+/*
 
 {{ config(materialized='table') }}
 
@@ -22,8 +25,11 @@ select *
 
 from source_data
 
+*/
+
 /*
     Uncomment the line below to remove records with null `id` values
 */
 
 -- where id is not null
+
